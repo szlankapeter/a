cd5d52ab31178e2ba592d602f43f523dbc7f98c0
szlankapeter <szlankapeter@gmail.com>
2023-09-25 15:15:08 +0200

    elso

---
+++ src/main/java/com/mycompany/a/A.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.a;
+
+/**
+ *
+ * @author szlanka.peter
+ */
+public class A {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("");
+        
+        
+    }
+}
